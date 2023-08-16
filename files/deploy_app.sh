e62da591682b17cb5da53bf4e00f543041ce819d
ShahidQavi <41574536+sqavi@users.noreply.github.com>
2023-08-16 12:52:16 +0100

    Update deploy_app.sh
    chaged

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to Sqavi's app. Replace this text with your own.
+  Welcome to Sqavi's app. Let's see how it works.
   <!-- END -->
 
   </div>
