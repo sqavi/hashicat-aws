166afcb041275f7a547f3bca81163d03daca428c
ShahidQavi <41574536+sqavi@users.noreply.github.com>
2023-08-16 15:16:50 +0100

    Update s3-bucket.tf
    try

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,8 +1,6 @@
 module "s3-bucket" {
   source  = "app.terraform.io/sqavi-org/s3-bucket/aws"
   version = "2.8.0"
-}
-
   bucket        = "s3-bucket"
   acl           = "private"
   bucket_prefix = "shahid"
