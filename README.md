ba03a357f5f2cd806b6cbba33edc7d3f3bbea6c5
Shahid Qavi <shahidqavi@gmail.com>
2023-08-16 12:20:43 +0000

    Added the first tag

--- README.md
+++ README.md
@@ -4,8 +4,5 @@ Hashicat: A terraform built application for use in Hashicorp workshops
 Includes "Meow World" website.
 
 [![infrastructure-tests](https://github.com/hashicorp/hashicat-aws/actions/workflows/infrastructure-tests.yml/badge.svg)](https://github.com/hashicorp/hashicat-aws/actions/workflows/infrastructure-tests.yml)
-<<<<<<< HEAD
-Shahid
-=======
+
 Shahid Qavi
->>>>>>> 2a685a44101fbab76b655de3cf6597804b6470f6
--- remote_backend.tf
+++ remote_backend.tf
@@ -1,22 +1,18 @@
-<<<<<<< HEAD
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "sqavi-org"
     workspaces {
       name = "hashicat-aws"
     }
   }
 }
-=======
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "sqavi-training"
     workspaces {
       name = "hashicat-aws"
     }
   }
 }
-
->>>>>>> 2a685a44101fbab76b655de3cf6597804b6470f6
